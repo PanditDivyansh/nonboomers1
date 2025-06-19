b41c16ac7089461e9cc1e7fb4d303f4b0ec8c9ff
PanditDivyansh <92201174+PanditDivyansh@users.noreply.github.com>
2025-06-19 19:06:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Landing from './components/Landing'
 import leaf3 from './assets/leaves/leaf3.png'
 import Marquee from './components/Marquee'
-import Index from './components'
+import Index from './components/Index'
 
 
 const App = () => {
