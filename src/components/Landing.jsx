3cb46744ddf1b0f0f0756045e31c83b8b750c8c6
PanditDivyansh <92201174+PanditDivyansh@users.noreply.github.com>
2025-06-25 14:15:57 +0530

    Update Landing.jsx

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -25,10 +25,7 @@ const Landing = () => {
         src={leaf2}
         alt="Falling Leaf"
 
-        className="absolute  sm:top-40  sm:left-[75%] left-[10%] top-[35%] z-100  sm:w-90 w-60 pointer-events-none opacity-90 drop-shadow-[0_8px_6px_rgba(0,0,0,0.3)]"
-
-        className="absolute  sm:top-40  sm:left-[75%] left-[10%] top-[50%] z-100  sm:w-90 w-60 pointer-events-none opacity-90 drop-shadow-[0_8px_6px_rgba(0,0,0,0.3)]"
-
+        className="absolute  sm:top-40  sm:left-[75%] left-[10%] top-[30%] z-100  sm:w-90 w-60 pointer-events-none opacity-90 drop-shadow-[0_8px_6px_rgba(0,0,0,0.3)]"
         initial={{ y: 0, x: 0, rotate: 0, opacity: 1 }}
         animate={{
           y: [0, 60, 140, 220, 300],
